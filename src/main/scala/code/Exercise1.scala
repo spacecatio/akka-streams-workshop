43840101e4ae962a12c5151afd505d5a5ef1bcb9
Dave Gurnell <dave@cartographer.io>
2020-06-08 07:41:27 +0100

    Fix merge conflict

--- src/main/scala/code/Exercise1.scala
+++ src/main/scala/code/Exercise1.scala
@@ -11,7 +11,6 @@ object Exercise1 extends App {
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
-<<<<<<< HEAD
 
   val numbers: Source[Int, NotUsed] =
     Source(1 to 100)
@@ -23,6 +22,4 @@ object Exercise1 extends App {
     numbers.toMat(printValues)(Keep.right)
 
   graph.run().onComplete(_ => system.terminate())
-=======
->>>>>>> 90de059... Created exercise descriptions
 }
