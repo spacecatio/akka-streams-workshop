f6c5de84e9285dc025fc8ae8ce34e85c823a9eb6
Dave Gurnell <dave@cartographer.io>
2020-06-08 08:25:33 +0100

    Add Exercise0.scala for experiments not related to other exercises

--- src/main/scala/code/Exercise0.scala
+++ src/main/scala/code/Exercise0.scala
@@ -1,3 +1,21 @@
-package code class Exercise0 {
+package code
 
+import akka.{Done, NotUsed}
+import akka.actor.ActorSystem
+import akka.stream._
+import akka.stream.scaladsl._
+
+import scala.concurrent._
+import scala.concurrent.duration._
+
+object Exercise0 extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  // Recipe:
+  //  1. Create source
+  //  2. Create sink
+  //  3. Connect source to sink
+  //  4. Run (and maybe collect final value)
 }
